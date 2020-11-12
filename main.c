0f6896400446933dc17b81af5e058bbd7805a47b
facu2279 <2279@holbertonschool.com>
2020-11-12 13:58:40 +0000

    second_day

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 /**
- * main - shell is a command-line interpreter which exposes access to an operating system's services
+ * main - shell is a command-line interpreter which
+ * exposes access to an operating system's services
+ * Return 0
  */
-int main()
+int main(void)
 {
 
+	return (0);
 }
